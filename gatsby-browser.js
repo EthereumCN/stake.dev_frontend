000dea26b1ee253274739fd2998b5d06a7f3493f
ECN | Ethereum.cn <eth@ecn.co>
2021-06-15 10:45:37 +0800

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ChakraProvider } from "@chakra-ui/react"
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ChakraProvider>
+      {element}
+    </ChakraProvider>
+  )
+}
